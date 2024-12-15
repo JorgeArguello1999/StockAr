044a2523bda9300d305b5a8f04e90440b241fd84
JorgeArguello1999 <jorge.arguello1999@gmail.com>
2024-12-14 21:51:00 -0500

    feat: Add basic structure for database files

--- func/core.py
+++ func/core.py
@@ -12,18 +12,19 @@ file_path = "./database/"
 makedirs(file_path, exist_ok=True) # Exist?
 
 # Files
-files = [
-    'cashier', # Cashier (User Data)
-    'customers', # Customers (User Data)
-    'data' # Data (Inventory Data)
-]
+files = {
+    'cashier': "id, user, password", # Cashier (User Data)
+    'customers': "id, name, item, date, price", # Customers (User Data)
+    'data': "id, name, price, count, total_value" # Data (Inventory Data)
+}
 
 # Create files
-def create_files(file_name:str) -> bool:
+def create_files(file_name:str, rows:str) -> bool:
     "Create your files if don't exist"
 
     try: 
         with open(f"{file_path}/{file_name}.txt", 'a') as file:
+            file.write(f'{rows}\n')
             file.close()
         return True 
 
@@ -33,12 +34,12 @@ def create_files(file_name:str) -> bool:
         return False
 
 # Start Files 
-def start_files(files:list=files) -> None: 
+def start_files(files:dict=files) -> None: 
     "Start all files for app"
 
-    for i in files:
+    for i in files.keys():
         if isfile(f'{file_path}/{i}.txt'):
             logger.log_info(f'{i}.txt Exist')
         else: 
             logger.log_warning(f'{i}.txt No exist, creating... ')
-            create_files(i)
+            create_files(i, files[i])
